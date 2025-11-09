71c6ee0ab4648e27eb7a289b41a3d6e225cfe7a8
Faycal <bz.faycal@gmail.com>
2025-11-09 13:03:57 +0100

    refactor(version): extract version info to dedicated package

--- cmd/version.go
+++ cmd/version.go
@@ -2,39 +2,15 @@ package cmd
 
 import (
 	"fmt"
-	"runtime"
 
+	"github.com/fbz-tec/pgxport/internal/version"
 	"github.com/spf13/cobra"
-)
-
-// Version information (set via ldflags during build)
-var (
-	Version   = "dev"
-	BuildTime = "unknown"
-	GitCommit = "unknown"
 )
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print version information",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(GetVersionInfo())
+		fmt.Println(version.GetInfo())
 	},
-}
-
-// GetVersionInfo returns formatted version information
-func GetVersionInfo() string {
-	return fmt.Sprintf(
-		`Version: %s
-Build time: %s
-Git commit: %s
-Go version: %s
-OS/Arch: %s/%s`,
-		Version,
-		BuildTime,
-		GitCommit,
-		runtime.Version(),
-		runtime.GOOS,
-		runtime.GOARCH,
-	)
 }
